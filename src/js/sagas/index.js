50e8b0d5203c63a8d652c1fc0b602ba80e6e1dea
Samyak Singh <samyasin@visa.com>
2017-11-01 19:58:13 -0700

    Plain form with no enhancements

--- src/js/sagas/index.js
+++
@@ -1,5 +0,0 @@
-import { watchEventChannel } from './PrefillSaga';
-
-export default function* rootSaga(dispatch) {
-  yield [ watchEventChannel() ];
-}
--- src/js/store.js
+++ src/js/store.js
@@ -1,13 +1,9 @@
 import createSagaMiddleware from 'redux-saga';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore } from 'redux';
 import reducers from './reducers';
-import rootSaga from './sagas';
 
-const sagaMiddleware = createSagaMiddleware();
 const store = createStore(
-  reducers,
-  applyMiddleware(sagaMiddleware)
+  reducers
 );
-sagaMiddleware.run(rootSaga, store.dispatch);
 
 export default store;
